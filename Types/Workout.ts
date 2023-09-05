84c2c35b317bbdb463e627c37f6a711db88cf566
j-carlander <johannes.carlander@gmail.com>
2023-09-05 11:13:40 +0200

    Type for workout

---
+++ Types/Workout.ts
@@ -0,0 +1,17 @@
+import { ObjectId } from "mongodb";
+import { User } from "./User";
+
+export type WorkoutType = "Spinning" | "Aerobics" | "Gympa" | "Dans" | "Yoga";
+export type City = "Norrtälje" | "Åkersberga";
+export type Recurring = "just_once" | "every_week" | "every_other_week";
+
+export interface Workout {
+  _id?: ObjectId;
+  workoutType: WorkoutType;
+  city: City;
+  participants: User[];
+  startTime: Date;
+  maxAllowedParticipants: number;
+  recurring: Recurring;
+  durationInMin: number;
+}
