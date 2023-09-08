f4c6a2c8e7c66516825296b8de2d07ffc526b548
j-carlander <johannes.carlander@gmail.com>
2023-09-08 11:03:44 +0200

    datepicker now works as a filter for workoutlist

--- src/Components/WorkoutsList/WorkoutsList.tsx
+++ src/Components/WorkoutsList/WorkoutsList.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Workout } from "../../../Types/Workout";
+import { FilterOptions, Workout } from "../../../Types/Workout";
 import WorkoutsCardComponent from "../workoutsCardComponent/WorkoutsCardComponent";
 import fetchService, { PatchAction } from "../../service/fetchService";
 import memoryService from "../../service/memoryService";
@@ -7,7 +7,11 @@ import { User } from "../../../Types/User";
 
 const currentUser = memoryService.getSessionValue("USER_INFO") as User;
 
-export function WorkoutsList(): JSX.Element {
+interface WorkoutListProps {
+  filter: FilterOptions;
+}
+
+export function WorkoutsList({ filter }: WorkoutListProps): JSX.Element {
   const [workouts, setWorkouts] = useState([] as Workout[]);
 
   useEffect(() => {
@@ -38,34 +42,23 @@ export function WorkoutsList(): JSX.Element {
     fetchService.getWorkouts().then(setWorkouts);
   }
 
-  // async function cancelWorkout(workout: Workout): Promise<void> {
-  //   if (!workout._id || !currentUser) return;
-
-  //   const response = await fetchService.patchWorkout(
-  //     workout._id,
-  //     currentUser,
-  //     "CANCEL"
-  //   );
-  //   if (response.status === 200) {
-  //     currentUser.bookedWorkouts = currentUser.bookedWorkouts.filter(
-  //       (booking) => booking !== workout._id
-  //     );
-  //     memoryService.saveSessionValue("USER_INFO", currentUser);
-  //   }
-  // }
-
   return (
     <>
       {workouts.length > 0 &&
-        workouts.map((workout) => (
-          <WorkoutsCardComponent
-            key={workout._id}
-            workout={workout}
-            currentUser={currentUser}
-            handleWorkout={handleWorkout}
-            // cancelWorkout={cancelWorkout}
-          />
-        ))}
+        workouts
+          .filter((workout) =>
+            currentUser.role === "ADMIN"
+              ? workout
+              : filter.date.getDate() === new Date(workout.startTime).getDate()
+          )
+          .map((workout) => (
+            <WorkoutsCardComponent
+              key={workout._id}
+              workout={workout}
+              currentUser={currentUser}
+              handleWorkout={handleWorkout}
+            />
+          ))}
     </>
   );
 }
