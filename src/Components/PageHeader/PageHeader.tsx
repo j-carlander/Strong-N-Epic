e1010f5caaf4cceedaeedde2226355a1869b7739
j-carlander <johannes.carlander@gmail.com>
2023-09-11 09:27:13 +0200

    Links for reg and log in moved from page header to homepage

--- src/Components/PageHeader/PageHeader.tsx
+++ src/Components/PageHeader/PageHeader.tsx
@@ -1,9 +1,8 @@
 import { useRef } from "react";
-import { Location, NavLink, useLocation } from "react-router-dom";
+import { Location, NavLink, useLocation, useNavigate } from "react-router-dom";
 import styles from "./PageHeader.module.css";
 import blackLogo from "../../assets/img/SiteLogoBlack.png";
 import memoryService from "../../service/memoryService";
-
 
 export function PageHeader(): JSX.Element {
   const dialogRef = useRef<HTMLDialogElement>(null);
@@ -20,31 +19,33 @@ export function PageHeader(): JSX.Element {
     navigate("/login");
   }
 
-  // currentUser.role === "ADMIN" ? styles["sign-out-admin-btn"] : styles["sign-out-user-btn"] 
+  // currentUser.role === "ADMIN" ? styles["sign-out-admin-btn"] : styles["sign-out-user-btn"]
 
   const currentLocation: Location = useLocation();
   return (
     <header className={styles.header}>
       <h1 className={styles["page-title"]}>Strong'N'Epic</h1>
-      {currentLocation.pathname !== "/" && currentLocation.pathname !== "/login" && currentLocation.pathname !== "/register" && (
-        <div className={styles["top-bar"]}>
-          {currentUser.role === "ADMIN" && (
-            <div>
-              <button
-                className={styles["open-menu-btn"]}
-                onClick={() => {
-                  dialogRef.current?.show();
-                }}>
-                &#9776;
-              </button>
-              <dialog className={styles.dialog} ref={dialogRef}>
+      {currentLocation.pathname !== "/" &&
+        currentLocation.pathname !== "/login" &&
+        currentLocation.pathname !== "/register" && (
+          <div className={styles["top-bar"]}>
+            {currentUser.role === "ADMIN" && (
+              <div>
                 <button
-                  className={styles["close-menu-btn"]}
+                  className={styles["open-menu-btn"]}
                   onClick={() => {
-                    dialogRef.current?.close();
+                    dialogRef.current?.show();
                   }}>
-                  &#10006;
+                  &#9776;
                 </button>
+                <dialog className={styles.dialog} ref={dialogRef}>
+                  <button
+                    className={styles["close-menu-btn"]}
+                    onClick={() => {
+                      dialogRef.current?.close();
+                    }}>
+                    &#10006;
+                  </button>
                   <nav>
                     <ul>
                       <li>
@@ -55,23 +56,26 @@ export function PageHeader(): JSX.Element {
                       </li>
                     </ul>
                   </nav>
-              </dialog>
+                </dialog>
+              </div>
+            )}
+            <div
+              className={
+                currentUser.role === "ADMIN"
+                  ? styles["current-user-admin-options"]
+                  : styles["current-user-user-options"]
+              }>
+              <p className="currently-logged-in">
+                Currently logged in as {currentUser.username}{" "}
+              </p>
+              <button className={styles["sign-out-btn"]} onClick={logOut}>
+                <i className="fa fa-sign-out"></i>
+              </button>
             </div>
-          )}
-          <div className={currentUser.role === "ADMIN" ? styles["current-user-admin-options"] : styles["current-user-user-options"]}>
-            <p className="currently-logged-in">Currently logged in as {currentUser.username} </p>
-            <button className={styles["sign-out-btn"]} onClick={logOut}>
-              <i className="fa fa-sign-out"></i>
-            </button>
           </div>
-        </div>
-      )}
+        )}
       <div>
         <img className={styles.logo} src={blackLogo} alt="Strong'N'Epic" />
-      </div>
-      <div className={styles.logRegContainer}>
-        <NavLink to={"/register"}><button className={styles.logRegBtn}>Register</button></NavLink>
-        <NavLink to={"/login"}><button className={styles.logRegBtn}>Login</button></NavLink>
       </div>
     </header>
   );
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,54 +1,57 @@
-// import { useState } from "react";
-// import LoginComponent from "../Components/LoginComponent/LoginComponent";
-// import RegisterComponent from "../Components/RegisterComponent/RegisterComponent";
-// import { useState } from "react";
-// import LoginComponent from "../Components/LoginComponent/LoginComponent";
-// import RegisterComponent from "../Components/RegisterComponent/RegisterComponent";
-
-// import { FormState } from "../../Types/Form.js";
-import {  workoutTypes } from "../../Types/Workout.js";
-import {  cities } from "../../Types/Workout.js";
+import { NavLink } from "react-router-dom";
+import { workoutTypes } from "../../Types/Workout.js";
+import { cities } from "../../Types/Workout.js";
 import { PageHeader } from "../Components/PageHeader/PageHeader.js";
 
-type Props = {
-  loggedIn: boolean;
-};
-
-export function HomePage({ loggedIn }: Props): JSX.Element {
+export function HomePage(): JSX.Element {
   // const [formState, setFormState] = useState("LOGIN" as FormState);
-  
+
   return (
     <>
-      <PageHeader loggedIn={loggedIn} />
-
-      <article style={{ textAlign: 'center'}}>
-        <h1>Welcome to the Strong-N-Epic Gym: Where We Turn Keyboard Warriors into Iron Warriors!</h1>
-        <p>Hey there, fearless trolls of the interwebs! 🖥️ Are your "gym sessions" limited to lifting bags of chips and slamming energy drinks? Well, it's time to upgrade those puny muscles and transform into the ultimate meme machine! 💥</p>
+      <PageHeader />
+      <div className="logRegContainer">
+        <NavLink to={"/register"}>
+          <button className="logRegBtn">Register</button>
+        </NavLink>
+        <NavLink to={"/login"}>
+          <button className="logRegBtn">Login</button>
+        </NavLink>
+      </div>
+      <article style={{ textAlign: "center" }}>
+        <h1>
+          Welcome to the Strong-N-Epic Gym: Where We Turn Keyboard Warriors into
+          Iron Warriors!
+        </h1>
+        <p>
+          Hey there, fearless trolls of the interwebs! 🖥️ Are your "gym
+          sessions" limited to lifting bags of chips and slamming energy drinks?
+          Well, it's time to upgrade those puny muscles and transform into the
+          ultimate meme machine! 💥
+        </p>
         <br />
-        <p>At Strong-N-Epic, we've designed a workout plan that's as epic as your online roasts! 😂 Prepare to sweat, grunt, and flex your way to a whole new level of trolling supremacy. Here's what to expect:</p>
-            {workoutTypes.map((type) => {
-            return(<p>{type}</p>)
-          })}
+        <p>
+          At Strong-N-Epic, we've designed a workout plan that's as epic as your
+          online roasts! 😂 Prepare to sweat, grunt, and flex your way to a
+          whole new level of trolling supremacy. Here's what to expect:
+        </p>
+        {workoutTypes.map((type) => {
+          return <p>{type}</p>;
+        })}
         <br />
-        <p>So, gear up, grab your keyboard, and join Strong-N-Epic Gym today. We're not just building muscles; we're building memes that will go down in internet history. Get ready to flex your way to victory while looking fabulous doing it! 💪😎</p>
+        <p>
+          So, gear up, grab your keyboard, and join Strong-N-Epic Gym today.
+          We're not just building muscles; we're building memes that will go
+          down in internet history. Get ready to flex your way to victory while
+          looking fabulous doing it! 💪😎
+        </p>
         <br />
-        <p>Our locations: <br />
+        <p>
+          Our locations: <br />
           {cities.map((city) => {
-            return(`${city} `)
+            return `${city} `;
           })}
         </p>
       </article>
-
-      {/* {formState === "LOGIN" && (
-        <LoginComponent 
-          formState={formState}
-          setFormState={setFormState}
-        />
-      )}
-
-      {formState === "REGISTER" && (
-        <RegisterComponent formState={formState} setFormState={setFormState} />
-      )} */}
     </>
   );
 }
